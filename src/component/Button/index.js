111e7eeb5c071789c9c80abebcb241d35b08910b
SangLT20 <196letansang@gmail.com>
2022-12-10 01:42:33 +0700

    setup project

---
+++ src/component/Button/index.js
@@ -0,0 +1,9 @@
+import myAlert from "~"
+//import myAlert from "../../myAlert"
+import React from "react"
+
+function Button() {
+    return <button onClick={myAlert}>Click me!</button>
+}
+
+export default Button
