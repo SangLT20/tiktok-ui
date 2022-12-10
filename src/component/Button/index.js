e1b4ddd6ceadcfdbccd6d67a8c133dc38b44a1b6
SangLT20 <196letansang@gmail.com>
2022-12-10 23:04:34 +0700

    updating file code

--- src/component/Button/index.js
+++
@@ -1,9 +0,0 @@
-import myAlert from '../../myAlert.js';
-//import myAlert from "../../myAlert"
-import React from 'react';
-
-function Button() {
-    return <button onClick={myAlert}>Click me!</button>;
-}
-
-export default Button;
