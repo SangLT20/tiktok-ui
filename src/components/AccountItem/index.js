9c413e5210bb375bf3c7f999cf3b25cedf9c7508
SangLT20 <196letansang@gmail.com>
2022-12-16 17:02:50 +0700

    adding button

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -8,11 +8,7 @@ const cx = classNames.bind(style);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
-                className={cx('avata')}
-                src="https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/8d0ad497032799c6a28e0127c1c9a89c~c5_100x100.jpeg?x-expires=1671004800&x-signature=EAJubwY%2B15Z2XonSaftL9WrR3Wc%3D"
-                alt=""
-            />
+            <img className={cx('avata')} src="http://localhost/100x100.jpeg" alt="" />
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyeen Van A</span>
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,16 @@
-import styles from './Header.module.scss';
-import classNames from 'classnames/bind';
-import images from '~/assets/images';
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 //import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
 import Tippy from '@tippyjs/react/headless';
+import classNames from 'classnames/bind';
+
+import styles from './Header.module.scss';
+import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
+import Button from '~/components/Button';
 
 const cx = classNames.bind(styles);
 
@@ -53,7 +55,10 @@ function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>Log in</Button>
+                </div>
             </div>
         </header>
     );
